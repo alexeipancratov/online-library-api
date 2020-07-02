18fd9d6408188aba6f2ab9baead74f7be73363b9
Alexei Pancratov <mixcube7@gmail.com>
2020-07-02 21:47:10 +0300

    Initial

---
+++ src/OnlineLibrary.Domain/Book.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OnlineLibrary.Data
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+    }
+}
---
+++ src/OnlineLibrary.WebApi/Controllers/BooksController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OnlineLibrary.Data;
+
+namespace OnlineLibrary.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly ILogger<BooksController> _logger;
+
+        public BooksController(ILogger<BooksController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return new Book[]
+            {
+                new Book { Id = 1, Title = "A Brief History of Time" },
+                new Book { Id = 2, Title = "Cooking Book by Martha" },
+                new Book { Id = 3, Title = "Where to travel?" }
+            };
+        }
+    }
+}
